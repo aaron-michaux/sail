39032f5fba5b1494181e9010e096c695bb069d7f
Aaron Michaux <aaron.michaux@broadcom.com>
2021-12-11 13:55:07 -0500

    removed stray file

--- tests/scandeps/02/speech_spanish.cpp.d
+++
@@ -1,4 +0,0 @@
-speech_spanish.o gcm.cache/speech-spanish.gcm: speech_spanish.cpp
-speech:spanish.c++m: gcm.cache/speech-spanish.gcm
-.PHONY: speech:spanish.c++m
-gcm.cache/speech-spanish.gcm:| speech_spanish.o
